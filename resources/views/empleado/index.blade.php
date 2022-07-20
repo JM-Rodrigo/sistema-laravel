eae514ad6389bf34344c1ddb4b7592971558ae21
Rodrigo <108javierr@gmail.com>
2022-07-20 13:44:57 -0500

    Estructura y rutas

---
+++ resources/views/empleado/index.blade.php
@@ -0,0 +1,18 @@
+<form action="" method="post" enctype="multipart/form-data">
+    <label for="Nombre">Escribe el nombre:</label>
+    <input type="text" name="Nombre" id="Nombre">
+    <br>
+    <label for="ApellidoPaterno">Escribe el apellido paterno:</label>
+    <input type="text" name="ApellidoPaterno" id="ApellidoPaterno">
+    <br>
+    <label for="ApellidoMaterno">Escribe el apellido materno:</label>
+    <input type="text" name="ApellidoMaterno" id="ApellidoMaterno" >
+    <br>
+    <label for="Correo">Escribe el correo electronico:</label>
+    <input type="email" name="Correo" id="Correo">
+    <br>
+    <label for="Foto">Elija la foto:</label>
+    <input type="file" name="Foto" id="Foto">
+    <br>
+    <input type="submit" name="Guardar" id="Guardar">
+</form>
